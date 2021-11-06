78ee5eb9fdd63b12a42777dc4d4048e0713c6f7c
andres <andres8sanchez@gmail.com>
2021-11-06 09:47:06 +0100

    Camera Rotation without Reference first commit

--- BrushlessEngine/src/ModuleCamera3D.cpp
+++ BrushlessEngine/src/ModuleCamera3D.cpp
@@ -15,6 +15,7 @@ ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(ap
 	cameraOffset = { 0.f, 4.f, 0.f };
 	Position = vec3(0.0f, 0.0f, 5.0f);
 	Reference = vec3(0.0f, 0.0f, 0.0f);
+	LookAtReference = vec3(0.0f, 0.0f, 0.0f);
 }
 
 ModuleCamera3D::~ModuleCamera3D()
@@ -45,19 +46,7 @@ update_status ModuleCamera3D::Update(float dt)
 	if(App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
 		speed = 3.0f * dt;
 
-	if(App->input->GetKey(SDL_SCANCODE_E) == KEY_REPEAT) newPos.y -= speed;
-	if(App->input->GetKey(SDL_SCANCODE_Q) == KEY_REPEAT) newPos.y += speed;
-	
-	if(App->input->GetKey(SDL_SCANCODE_W) == KEY_REPEAT) newPos -= Z * speed;
-	if(App->input->GetKey(SDL_SCANCODE_S) == KEY_REPEAT) newPos += Z * speed;
-
-
-	if(App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) newPos -= X * speed;
-	if(App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) newPos += X * speed;
-
-
 	//// Mouse motion ----------------
-
 	if(App->input->GetMouseButton(SDL_BUTTON_MIDDLE) == KEY_REPEAT)
 	{
 		int dx = App->input->GetMouseXMotion();
@@ -78,9 +67,12 @@ update_status ModuleCamera3D::Update(float dt)
 
 			newPos += Y * DeltaY;
 		}
+
+		Move(newPos);
 	}
 
-	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT && App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT)
+	//rotation with reference
+	if (App->input->GetKey(SDL_SCANCODE_LSHIFT) == KEY_REPEAT && App->input->GetMouseButton(SDL_BUTTON_LEFT) == KEY_REPEAT)
 	{
 		int dx = -App->input->GetMouseXMotion();
 		int dy = -App->input->GetMouseYMotion();
@@ -115,8 +107,40 @@ update_status ModuleCamera3D::Update(float dt)
 		Position = Reference + Z * length(Position);
 	}
 
-	Position += newPos;
-	Reference += newPos;
+
+	//rotation look at
+	if (App->input->GetMouseButton(SDL_BUTTON_RIGHT) == KEY_REPEAT)
+	{
+		int dx = -App->input->GetMouseXMotion();
+		int dy = -App->input->GetMouseYMotion();
+
+		float Sensitivity = 0.2f;
+		
+
+		if (dx != 0)
+		{
+			float DeltaX = (float)dx * Sensitivity;
+
+			X = rotate(X, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Y = rotate(Y, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			Z = rotate(Z, DeltaX, vec3(0.0f, 1.0f, 0.0f));
+			//Sensitivity -= DeltaX;
+		}
+
+		if (dy != 0)
+		{
+			float DeltaY = (float)dy * Sensitivity;
+
+			//Y = rotate(Y, DeltaY, vec3(0.0f, 1.0f, 0.0f));
+			//Z = rotate(Z, DeltaY, vec3(0.0f, 1.0f, 0.0f));
+			
+		}
+
+		//Position = Reference - Z * length(Position);
+	}
+
+
+	
 
 	// Recalculate matrix -------------
 	CalculateViewMatrix();
@@ -151,11 +175,10 @@ void ModuleCamera3D::LookAt( const vec3 &Spot)
 {
 	Reference = Spot;
 
-	Z = normalize(Position - Reference);
+	Z = normalize(Position - Spot);
 	X = normalize(cross(vec3(0.0f, 1.0f, 0.0f), Z));
 	Y = cross(Z, X);
 
-	CalculateViewMatrix();
 }
 
 
